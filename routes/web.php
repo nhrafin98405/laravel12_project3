2c8f2a2b697742d78cf3d3547bdd5857cf67d9b9
NH Rafin <nhrafin98405@gmail.com>
2026-06-22 13:38:07 +0600

    add

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,17 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
+
+Route::prefix('admin')->middleware('auth')->group(function () {
+    
+    Route::resource('product',ProductController::class);
+
+
+
+});
+
+
 
 require __DIR__.'/auth.php';
